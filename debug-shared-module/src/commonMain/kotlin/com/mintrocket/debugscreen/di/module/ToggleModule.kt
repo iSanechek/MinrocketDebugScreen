e8d79e8fca2c8cda33d04e3cf00f5d4f1a50a679
loganov <loganovdev@gmail.com>
2022-01-08 03:00:23 +0400

    add DebugFeaturesStateRepository implementation for ios

--- debug-shared-module/src/commonMain/kotlin/com/mintrocket/debugscreen/di/module/ToggleModule.kt
+++ debug-shared-module/src/commonMain/kotlin/com/mintrocket/debugscreen/di/module/ToggleModule.kt
@@ -7,7 +7,12 @@ import com.mintrocket.debugscreen.data.feature_toggling.provider.FeatureConfigsP
 import com.mintrocket.debugscreen.data.feature_toggling.provider.impl.FeatureConfigsProviderImpl
 import com.mintrocket.debugscreen.data.feature_toggling.repository.remote.RemoteFeaturesRepository
 import com.mintrocket.debugscreen.data.feature_toggling.repository.remote.impl.RemoteFeaturesRepositoryImpl
+import com.mintrocket.debugscreen.data.feature_toggling.repository.state.FeaturesStateRepository
+import com.mintrocket.debugscreen.data.feature_toggling.repository.state.impl.ReleaseFeaturesStateRepositoryImpl
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+
+val RELEASE_FEATURE_CONFIG_REPOSITORY = named("RELEASE_FEATURE_CONFIG_REPOSITORY")
 
 val toggleModule = module {
 
@@ -19,6 +24,10 @@ val toggleModule = module {
 
     single<RemoteFeaturesRepository> {
         RemoteFeaturesRepositoryImpl(get())
+    }
+
+    single<FeaturesStateRepository>(RELEASE_FEATURE_CONFIG_REPOSITORY) {
+        ReleaseFeaturesStateRepositoryImpl()
     }
 
 }
