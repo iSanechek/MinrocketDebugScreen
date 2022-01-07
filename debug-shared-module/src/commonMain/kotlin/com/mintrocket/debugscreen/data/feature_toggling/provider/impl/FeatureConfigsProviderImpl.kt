8109ef5de90c32a54b39cd24556f75ed3d2a0afc
loganov <loganovdev@gmail.com>
2022-01-07 22:42:51 +0400

    add FeatureConfigsProvider implementation

--- debug-shared-module/src/commonMain/kotlin/com/mintrocket/debugscreen/data/feature_toggling/provider/impl/FeatureConfigsProviderImpl.kt
+++ debug-shared-module/src/commonMain/kotlin/com/mintrocket/debugscreen/data/feature_toggling/provider/impl/FeatureConfigsProviderImpl.kt
@@ -1,5 +1,6 @@
 package com.mintrocket.debugscreen.data.feature_toggling.provider.impl
 
+import com.mintrocket.debugscreen.data.feature_toggling.FeatureEnabledState
 import com.mintrocket.debugscreen.data.feature_toggling.features.FeaturesDefaults
 import com.mintrocket.debugscreen.data.feature_toggling.features.toggles.LocalFeatureToggle
 import com.mintrocket.debugscreen.data.feature_toggling.features.toggles.RemoteFeatureToggle
@@ -12,11 +13,17 @@ class FeatureConfigsProviderImpl(
     private val remoteFeaturesRepository: RemoteFeaturesRepository,
     private val featuresStateRepository: FeaturesStateRepository
 ) : FeatureConfigsProvider {
-    override fun isLocalEnabled(toggle: LocalFeatureToggle): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isLocalEnabled(toggle: LocalFeatureToggle): Boolean =
+        when (featuresStateRepository.getState(toggle.key)) {
+            FeatureEnabledState.DEFAULT -> featuresDefaults.getLocalDefault(toggle)
+            FeatureEnabledState.ENABLED -> true
+            FeatureEnabledState.DISABLED -> false
+        }
 
-    override fun isRemoteEnabled(toggle: RemoteFeatureToggle): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isRemoteEnabled(toggle: RemoteFeatureToggle): Boolean =
+        when (featuresStateRepository.getState(toggle.key)) {
+            FeatureEnabledState.ENABLED -> true
+            FeatureEnabledState.DISABLED -> false
+            FeatureEnabledState.DEFAULT -> remoteFeaturesRepository.isEnabled(toggle.key)
+        }
 }
