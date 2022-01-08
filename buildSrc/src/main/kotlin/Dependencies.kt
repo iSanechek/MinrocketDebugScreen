0096e65d0b58f6e0bfb50e6ee60cd506199c470d
loganov <loganovdev@gmail.com>
2022-01-09 03:06:44 +0400

    refactoring dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,11 +1,66 @@
 
 object Versions {
-    const val koin = "3.1.4"
+    internal const val koin = "3.1.4"
+    internal const val coreKtxVersion = "1.7.0"
+    internal const val appCompatVersion = "1.4.0"
+    internal const val activityComposeVersion = "1.4.0"
+    internal const val materialVersion = "1.4.0"
+    internal const val composeVersion = "1.1.0-rc01"
+    internal const val junitVersion = "4.13.2"
+    internal const val junitExtVersion = "1.1.3"
+    internal const val espressoVersion = "3.4.0"
 }
 
 object Libs {
+
+    object Testing {
+        const val junit = "junit:junit:${Versions.junitVersion}"
+        const val junitExt = "androidx.test.ext:${Versions.junitExtVersion}"
+        const val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+    }
+
     object Koin {
         const val android = "io.insert-koin:koin-android:${Versions.koin}"
         const val core = "io.insert-koin:koin-core:${Versions.koin}"
     }
+
+    object AndroidX {
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+        const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtxVersion}"
+    }
+
+    object Material {
+        const val material = "com.google.android.material:material:${Versions.materialVersion}"
+    }
+
+    object Compose {
+        const val activity = "androidx.activity:activity-compose:${Versions.activityComposeVersion}"
+        const val runtime = "androidx.compose.runtime:runtime:${Versions.composeVersion}"
+        const val ui = "androidx.compose.ui:ui:${Versions.composeVersion}"
+        const val foundation = "androidx.compose.foundation:foundation:${Versions.composeVersion}"
+        const val foundationLayout = "androidx.compose.foundation:foundation-layout:${Versions.composeVersion}"
+        const val material = "androidx.compose.material:material:${Versions.composeVersion}"
+        const val animation = "androidx.compose.animation:animation:${Versions.composeVersion}"
+        const val tooling = "androidx.compose.ui:ui-tooling:${Versions.composeVersion}"
+    }
+
+    // Deps for screen module
+    val screenLibs = listOf(
+        with(AndroidX) {
+            appCompat
+            coreKtx
+        },
+        with(Compose) {
+            activity
+            runtime
+            ui
+            foundation
+            foundationLayout
+            material
+            animation
+            tooling
+        },
+        Material.material,
+        Koin.android
+    )
 }
--- debug-screen-module/build.gradle.kts
+++ debug-screen-module/build.gradle.kts
@@ -45,25 +45,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("com.google.android.material:material:1.4.0")
-
-    implementation("androidx.compose.runtime:runtime:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.foundation:foundation:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.foundation:foundation-layout:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.material:material-icons-extended:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.animation:animation:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.ui:ui-tooling:1.1.0-rc01")
-
     implementation(project(":debug-shared-module"))
-    implementation(Libs.Koin.android)
 
+    Libs.screenLibs.forEach(::implementation)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    testImplementation(Libs.Testing.junit)
+    androidTestImplementation(Libs.Testing.junitExt)
+    androidTestImplementation(Libs.Testing.espresso)
 }
